943a8f060a31b9e04d995f1a50f45ad256aac981
C-ccake <1142866160@qq.com>
2022-06-04 15:43:15 +0800

    ui最最后的优化

---
+++ src/views/home/creation/components/NetworkBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <router-link :to="{path:'/networkCreate/'+netId}">
+      <img :src="imgUrl">
+      <div>{{ netName }}</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NetworkBlock',
+  data () {
+    return {}
+  },
+  props: ['imgUrl', 'netName', 'netId']
+}
+</script>
+
+<style scoped>
+
+</style>
